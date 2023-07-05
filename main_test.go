0d2ca77a87f6e1f12ae29cbff657483fd471d743
coyotev <2020124chat@proton.me>
2023-07-05 03:16:04 +0000

    add_test by yoyolon

--- main_test.go
+++ main_test.go
@@ -4,6 +4,28 @@ import (
 	"testing"
 )
 
+// (0,0)に"o"を置くテスト
+func TestPutToken_00_o(t *testing.T) {
+	b := &Board{
+		tokens: []int{0, 0, 0, 0, 0, 0, 0, 0, 0},
+	}
+	b.put(0, 0, "o")
+	if b.get(0, 0) != "o" {
+		t.Errorf("....")
+	}
+}
+
+// (0,0)に"x"を置くテスト
+func TestPutToken_00_x(t *testing.T) {
+	b := &Board{
+		tokens: []int{0, 0, 0, 0, 0, 0, 0, 0, 0},
+	}
+	b.put(0, 0, "x")
+	if b.get(0, 0) != "x" {
+		t.Errorf("....")
+	}
+}
+
 // (1,1)に"o"を置くテスト
 func TestPutToken_11_o(t *testing.T) {
 	b := &Board{
@@ -22,6 +44,28 @@ func TestPutToken_11_x(t *testing.T) {
 	}
 	b.put(1, 1, "x")
 	if b.get(1, 1) != "x" {
+		t.Errorf("....")
+	}
+}
+
+// (2,2)に"x"を置くテスト
+func TestPutToken_22_x(t *testing.T) {
+	b := &Board{
+		tokens: []int{0, 0, 0, 0, 0, 0, 0, 0, 0},
+	}
+	b.put(2, 2, "x")
+	if b.get(2, 2) != "x" {
+		t.Errorf("....")
+	}
+}
+
+// (2,2)に"o"を置くテスト
+func TestPutToken_22_o(t *testing.T) {
+	b := &Board{
+		tokens: []int{0, 0, 0, 0, 0, 0, 0, 0, 0},
+	}
+	b.put(2, 2, "o")
+	if b.get(2, 2) != "o" {
 		t.Errorf("....")
 	}
 }
